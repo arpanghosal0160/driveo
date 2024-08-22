5215605e12447f939075ff5c2083ca4f12d93c03
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-08-22 20:19:11 +0530

    added tabs

---
+++ app/(root)/(tabs)/chat.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+import 'react-native-reanimated';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Chat = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Chat;
---
+++ app/(root)/(tabs)/rides.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+import 'react-native-reanimated';
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const Rides = () => {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Rides;
